57969b21b68a92ecd493aa15acd0be217d5c470e
Ronald Whittemore <nicebowtie@gmail.com>
2023-04-22 11:34:26 -0400

    Add files via upload

---
+++ Assets/JS/script.js
@@ -0,0 +1,19 @@
+
+let chicken = "100";
+console.log(chicken);
+
+chicken = Number(chicken);
+console.log(chicken);
+console.log(typeof chicken);
+
+let turkey = 100;
+console.log(turkey);
+
+let quail = turkey.toString();
+console.log(quail);
+console.log(typeof quail);
+
+console.log("In 59 years I will be " + chicken + " years old. " + quail + " is pretty old.");
+
+console.log(`In 59 years I will be ${turkey} years old.`);
+
